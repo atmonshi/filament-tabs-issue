b001fea42802a92b1a3c2a7fa60c267c204736d8
Ash Monsh <wh7r.com@gmail.com>
2025-06-14 17:14:02 +0300

    add relations

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Section extends Model
 {
-    //
+    public function form(): BelongsTo
+    {
+        return $this->belongsTo(Form::class);
+    }
 }
