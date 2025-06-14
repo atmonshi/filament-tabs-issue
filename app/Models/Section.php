b80e6087a1fbfdee644d891daa68314214e8baef
Ash Monsh <wh7r.com@gmail.com>
2025-06-14 17:18:16 +0300

    add field model and migration

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -4,11 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Section extends Model
 {
     public function form(): BelongsTo
     {
         return $this->belongsTo(Form::class);
+    }
+
+    public function fields(): HasMany
+    {
+        return $this->hasMany(Field::class);
     }
 }
